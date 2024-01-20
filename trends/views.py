a0125f887e1956fa7598c4e124b46e432157ab22
vivekn <vnautiyal24@gsb.columbia.edu>
2024-01-19 22:53:43 -0500

    Beautified version Please enter the commit message for your changes. Lines starting

--- trends/views.py
+++ trends/views.py
@@ -2,22 +2,27 @@ from pytrends.request import TrendReq
 from django.shortcuts import render
 
 def trending_view(request):
+    selected_region = request.GET.get('region')  # Get the selected region from the request
+
     pytrends = TrendReq(hl='en-US', tz=360)
-
-    # List of wine types
     kw_list = ["Cabernet Sauvignon", "Pinot Noir", "Chardonnay", "Merlot", "Sauvignon Blanc"]
-    pytrends.build_payload(kw_list, cat=0, timeframe='today 3-m', geo='', gprop='')
 
-    # Interest over time
+    # Check if a region is selected, otherwise default to 'US'
+    if not selected_region:
+        selected_region = 'US'
+
+    pytrends.build_payload(kw_list, cat=0, timeframe='today 3-m', geo=selected_region, gprop='')
     interest_over_time_df = pytrends.interest_over_time()
 
-    # Drop the 'isPartial' column
-    interest_over_time_df = interest_over_time_df.drop(labels=['isPartial'], axis='columns')
+    # Dropping 'isPartial' column
+    if 'isPartial' in interest_over_time_df.columns:
+        interest_over_time_df = interest_over_time_df.drop('isPartial', axis=1)
 
-    # Find the top three trending wines
+    # Sum up and sort values to get top 3
     trending_wines = interest_over_time_df.sum().sort_values(ascending=False).head(3)
 
     context = {
-        'trending_wines': trending_wines,
+        'selected_region': selected_region,
+        'trending_wines': trending_wines.to_dict(),
     }
     return render(request, 'trending.html', context)
